7939e3ce1388997dba04972fa3140e86c2d85db1
Krendelev <krendelev@gmail.com>
2018-09-22 02:40:19 +0300

    main features implemented

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Check whether site is alive and its domain will not expire soon.
 
-# Project Goals
+## How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+## Quick Start
+
+Pass `text` file with URLs as argument.
+
+```bash
+$ python check_sites_health.py urls.txt
+Alive Expire  URL
+True   False  https://www.python.org
+True   False  https://devman.org
+True   False  https://github.com
+```
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,41 @@
+import sys
+import datetime
+import urllib.parse
+import whois
+import requests
+
+
 def load_urls4check(path):
-    pass
+    with open(path) as file_handler:
+        return file_handler.read().splitlines()
+
 
 def is_server_respond_with_200(url):
-    pass
+    return requests.head(url).ok
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+    domain = urllib.parse.urlsplit(url).hostname
+    return whois.query(domain).expiration_date
+
+
+def is_expire_soon(exp_date):
+    soon = 30
+    return exp_date.date() < (
+        datetime.date.today() + datetime.timedelta(days=soon)
+        )
 
 if __name__ == '__main__':
-    pass
+    try:
+        urls = load_urls4check(sys.argv[1])
+    except IndexError:
+        exit('Please specify path to URL list')
+    except FileNotFoundError:
+        exit('File not found')
+    print('Alive Expire  URL')
+    for url in urls:
+        print('{0!s:6} {1!s:6} {2}'.format(
+            is_server_respond_with_200(url),
+            is_expire_soon(get_domain_expiration_date(url)),
+            url)
+        )
