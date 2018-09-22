33d82c42543367b63d53515c1423810d7272b4ef
Krendelev <krendelev@gmail.com>
2018-09-23 00:21:44 +0300

    add exceptions handling

--- check_sites_health.py
+++ check_sites_health.py
@@ -10,19 +10,23 @@ def load_urls4check(path):
         return file_handler.read().splitlines()
 
 
-def is_server_respond_with_200(url):
-    return requests.head(url).ok
+def is_server_respond_with_ok(url):
+    try:
+        response = requests.head(url, timeout=1)
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        return False
+    return response.ok
 
 
 def get_domain_expiration_date(url):
     domain = urllib.parse.urlsplit(url).hostname
-    return whois.query(domain).expiration_date
+    exp_date = whois.whois(domain).expiration_date
+    return exp_date[0] if type(exp_date) is list else exp_date
 
 
-def is_expire_soon(exp_date):
-    soon = 30
+def is_expire_in(interval, exp_date):
     return exp_date.date() < (
-        datetime.date.today() + datetime.timedelta(days=soon)
+        datetime.date.today() + datetime.timedelta(days=interval)
         )
 
 if __name__ == '__main__':
@@ -32,10 +36,13 @@ if __name__ == '__main__':
         exit('Please specify path to URL list')
     except FileNotFoundError:
         exit('File not found')
+    interval = 30
     print('Alive Expire  URL')
     for url in urls:
-        print('{0!s:6} {1!s:6} {2}'.format(
-            is_server_respond_with_200(url),
-            is_expire_soon(get_domain_expiration_date(url)),
-            url)
-        )
+        if is_server_respond_with_ok(url):
+            print('{1!s:6} {0!s:6} {2}'.format(
+                is_expire_in(interval, get_domain_expiration_date(url)),
+                True, url)
+            )
+        else:
+            print('Failed to establish connection to {}'.format(url))
