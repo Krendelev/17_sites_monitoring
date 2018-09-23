7e6516999bdb2894a651fc5d370da78161aaf560
Krendelev <krendelev@gmail.com>
2018-09-23 22:16:03 +0300

    refactor output

--- check_sites_health.py
+++ check_sites_health.py
@@ -13,26 +13,34 @@ def load_urls4check(path):
 def is_server_respond_with_ok(url):
     try:
         response = requests.head(url, allow_redirects=True, timeout=1)
+        return response.ok
     except requests.RequestException:
         return False
-    return response.ok
 
 
 def get_domain_expiration_date(url):
     domain = urllib.parse.urlsplit(url).hostname
     try:
         exp_date = whois.whois(domain).expiration_date
+        return exp_date[0] if isinstance(exp_date, list) else exp_date
     except whois.parser.PywhoisError:
         return None
-    return exp_date[0] if type(exp_date) is list else exp_date
 
 
 def is_expire_in(interval, exp_date):
     if not exp_date:
-        return 'N/A'
+        return None
     return exp_date.date() < (
         datetime.date.today() + datetime.timedelta(days=interval)
         )
+
+
+def print_status(url, is_alive, is_expire):
+    for value, status in {'True': 'Yes', 'False': 'No', 'None': 'N/A'}.items():
+        is_alive = str(is_alive).replace(value, status)
+        is_expire = str(is_expire).replace(value, status)
+    print('{0:6} {1:6} {2}'.format(is_alive, is_expire, url))
+
 
 if __name__ == '__main__':
     try:
@@ -44,8 +52,7 @@ if __name__ == '__main__':
     interval = 30
     print('Alive Expire  URL')
     for url in urls:
-        print('{1!s:6} {0!s:6} {2}'.format(
-            is_expire_in(interval, get_domain_expiration_date(url)),
-            is_server_respond_with_ok(url),
-            url)
+        print_status(
+            url, is_server_respond_with_ok(url),
+            is_expire_in(interval, get_domain_expiration_date(url))
         )
